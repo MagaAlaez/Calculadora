4668c2c67d34f3e27767e181fdd546bc3734ccd6
MagaAlaez <maguialaez@gmail.com>
2021-07-01 02:57:04 -0300

    error

--- src/Utils.test.js
+++ src/Utils.test.js
@@ -1,7 +1,7 @@
 import { Multiplicacion } from './Utils';
 
 test('multiplica bien', () => {
-  expect(Multiplicacion(1, 1)).toBe(2);
-  expect(Multiplicacion(2, 0)).toBe(0);
+  expect(Multiplicacion(1, 2)).toBe(2);
+  expect(Multiplicacion(2, 1)).toBe(0);
   expect(Multiplicacion(2, 2)).toBe(4);
 });
